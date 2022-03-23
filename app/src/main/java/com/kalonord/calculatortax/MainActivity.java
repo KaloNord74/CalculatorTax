2327bf190b480361aa43bf945473fd66b238260d
Denis Bogdanov <kalonord@yandex.ru>
2022-03-23 15:59:07 +0500

    Initial commit

--- app/src/main/java/com/kalonord/calculatortax/MainActivity.java
+++ app/src/main/java/com/kalonord/calculatortax/MainActivity.java
@@ -6,14 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int numberField;
-    private int ndfl;
-    private int oms;
-    private int pension;
-    private int social;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Button cancelButton = (Button) findViewById(R.id.cancelButton);
         Button okButton = (Button) findViewById(R.id.okButton);
         EditText editIncome = (EditText) findViewById(R.id.editIncome);
+        TextView ndfl = (TextView) findViewById(R.id.ndfl);
+        TextView oms = (TextView) findViewById(R.id.oms);
+        TextView pension = (TextView) findViewById(R.id.pension);
+        TextView social = (TextView) findViewById(R.id.social);
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                numberField = Integer.parseInt(editIncome.getText().toString());
+                ndfl.setText("Ваш подоходный налог: " + numberField / 100 * 13);
+                oms.setText("Ваши отчисления на медицинское страхование: " + numberField / 100 * 5.1);
+                pension.setText("Ваши отчисления на пенсионное страхование: " + numberField / 100 * 22);
+                social.setText("Ваши отчисления на обязательное социальное страхование в случае временной нетрудоспособности и в связи с материнством: " + numberField / 100 * 2.9);
             }
         });
 
